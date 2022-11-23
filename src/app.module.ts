ee567528746f1859c190c3ad20b3e6b5a6bb50c9
Varga Norbert <vnorbi02@gmail.com>
2022-11-23 12:36:47 +0100

    asd

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+class Emberek{
+  nev : string;
+  nem : string;
+  szul : Date;
+  egyenleg : string;
+  statusz : boolean;
+
+  constructor Emberek(nev,nem : string,szul : Date,egyenleg : string,statusz : boolean){
+      this.nev= nev;
+
+  }
+};
+
 @Module({
   imports: [],
   controllers: [AppController],
